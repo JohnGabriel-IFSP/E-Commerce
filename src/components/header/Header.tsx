bd296f3d3c08256e3a18420c16c1f2a958d39132
John Gabriel <john.gabriel@aluno.ifsp.edu.br>
2022-04-14 13:47:12 -0300

    Criando Navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,26 @@
-import { Center, Left, Navbar, Right, Wrapper } from "./style";
+import { PersonOutlineOutlined, Search, ShoppingCartOutlined } from "@material-ui/icons";
+
+import { Center, Input, Left, Logo, MenuContainer, Navbar, Right, SearchContainer, Wrapper } from "./style";
 
 export function Header(){
     return (
         <Navbar>
             <Wrapper>
-                <Left> Left </Left>
-                <Center> Center </Center>
-                <Right> Right </Right>
+                <Left>
+                    <Logo>Urban Store</Logo>
+                </Left>
+                <Center>
+                    <SearchContainer>
+                        <Input></Input>
+                        <Search></Search>
+                    </SearchContainer>
+                </Center>
+                <Right>
+                    <MenuContainer>
+                        <ShoppingCartOutlined></ShoppingCartOutlined>
+                        <PersonOutlineOutlined></PersonOutlineOutlined>
+                    </MenuContainer>
+                </Right>
             </Wrapper>
         </Navbar>
     );
--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
 export const Navbar = styled.div`
-    background-color: black;
+    //background: linear-gradient(to bottom, ,black,darkgrblackay, gray);
+    background-color: #121212;
     color: white;
-    height: 60px;
+    height: 120px;
 `;
 
 export const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 10px 20px;
 `;
 
@@ -16,10 +18,40 @@ export const Left = styled.div`
     flex: 1
 `;
 
+export const Logo = styled.h1`
+    font-family: 'Oswald', sans-serif;
+`;
+
+
 export const Center = styled.div`
-    flex: 1
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 2;
+`;
+
+export const SearchContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 0.5px solid lightgray;
+    width: 100%;
+    padding: 5px;
+`
+
+export const Input = styled.input`
+    border: none;
+    width: 100%;
 `;
 
 export const Right = styled.div` 
-    flex: 1
+    display: flex;
+    justify-content: flex-end;
+    flex: 1;
+`;
+
+export const MenuContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 5px;
 `;
