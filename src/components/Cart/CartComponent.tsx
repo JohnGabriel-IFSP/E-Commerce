5a9e1283ad065303bd0314b0dcec297d22748072
dijijn <nunes.jonathan@aluno.ifsp.edu.br>
2022-05-04 21:43:11 -0300

    Update CartComponent.tsx

--- src/components/Cart/CartComponent.tsx
+++ src/components/Cart/CartComponent.tsx
@@ -1,4 +1,4 @@
-import { ContentConteiner, LeftSide, RightSide, Title } from "./CartStyle"
+import { ContentConteiner, LeftSide, RightSide, Title, InputCEP, Button, DivFrete, Div, Text } from "./CartStyle"
 
 export const CartConteiner = () =>{
     return(
@@ -7,7 +7,13 @@ export const CartConteiner = () =>{
                 Meu Carrinho
             </Title>
             <LeftSide>
-
+                <DivFrete>
+                    <Text>Calcula frete e prazo</Text>
+                    <Div> 
+                        <InputCEP placeholder='Insira seu CEP'></InputCEP>
+                        <Button>ok</Button>
+                    </Div>
+                </DivFrete>
             </LeftSide>
             <RightSide>
 
