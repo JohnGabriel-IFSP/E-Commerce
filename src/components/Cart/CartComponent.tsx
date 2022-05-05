58783bc00c6cd35465662b6880b21345bb5be7e1
dijijn <nunes.jonathan@aluno.ifsp.edu.br>
2022-05-05 07:11:41 -0300

    Leftside Cart

--- src/components/Cart/CartComponent.tsx
+++ src/components/Cart/CartComponent.tsx
@@ -1,4 +1,7 @@
-import { ContentConteiner, LeftSide, RightSide, Title, InputCEP, Button, DivFrete, Div, Text } from "./CartStyle"
+import { Add, Remove } from "@mui/icons-material"
+import { ContentConteiner, LeftSide, RightSide, Title, InputCEP, Button, DivFrete, Div, Text, Bottom, 
+    Product, ProductDetail, Image, Details, ProductName, ProductId, ProductColor, ProductSize, ProductPrice, Top,
+    ProductAmountConteiner, ProductAmount, Hr, ProductPriceDetail } from "./CartStyle"
 
 export const CartConteiner = () =>{
     return(
@@ -6,7 +9,54 @@ export const CartConteiner = () =>{
             <Title>
                 Meu Carrinho
             </Title>
-            <LeftSide>
+            <Top>
+                <LeftSide>
+                    <Text>Produto</Text>
+                        <Product>
+                            <ProductDetail>
+                                <Image src='https://cf.shopee.com.br/file/71ad80f1c46275424f2f84cbe421537c'/>
+                                <Details>
+                                    <ProductName><b>Produto:</b> Camisa Masculina</ProductName>
+                                    <ProductId><b>ID:</b> 010101</ProductId>
+                                    <ProductColor color='black'/>
+                                    <ProductSize><b>Tamanho: </b> M</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                        <ProductPrice>
+                            <ProductAmountConteiner>
+                                <Add cursor='pointer'/>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove cursor='pointer'/>
+                            </ProductAmountConteiner>
+                            <ProductPriceDetail>R$ 49,99</ProductPriceDetail>
+                        </ProductPrice>
+                    </Product>
+                    <Hr/>
+                    <Product>
+                            <ProductDetail>
+                                <Image src='https://cf.shopee.com.br/file/71ad80f1c46275424f2f84cbe421537c'/>
+                                <Details>
+                                    <ProductName><b>Produto:</b> Camisa Masculina</ProductName>
+                                    <ProductId><b>ID:</b> 010101</ProductId>
+                                    <ProductColor color='black'/>
+                                    <ProductSize><b>Tamanho: </b> M</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                        <ProductPrice>
+                            <ProductAmountConteiner>
+                                <Add cursor='pointer'/>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove cursor='pointer'/>
+                            </ProductAmountConteiner>
+                            <ProductPriceDetail>R$ 49,99</ProductPriceDetail>
+                        </ProductPrice>
+                    </Product>
+                </LeftSide>
+                <RightSide>
+                    summary
+                </RightSide>
+            </Top>
+            <Bottom>
                 <DivFrete>
                     <Text>Calcula frete e prazo</Text>
                     <Div> 
@@ -14,10 +64,7 @@ export const CartConteiner = () =>{
                         <Button>ok</Button>
                     </Div>
                 </DivFrete>
-            </LeftSide>
-            <RightSide>
-
-            </RightSide>
+            </Bottom>
         </ContentConteiner>
     )
 }
--- src/components/Cart/CartStyle.tsx
+++ src/components/Cart/CartStyle.tsx
@@ -12,12 +12,21 @@ export const ContentConteiner = styled.div`
     box-sizing: border-box;
 `;
 
-export const LeftSide = styled.div`
+export const Top = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Bottom = styled.div`
 
 `;
 
-export const RightSide = styled.div`
+export const LeftSide = styled.div`
+    flex: 3;
+`;
 
+export const RightSide = styled.div`
+    flex: 1;
 `;
 
 export const Title = styled.h1`
@@ -43,3 +52,75 @@ export const Div = styled.div`
 export const Text = styled.span`
 
 `;
+
+export const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`;
+
+export const Image = styled.img`
+    width: 200px;
+`;
+
+export const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const ProductName = styled.span`
+
+`;
+
+export const ProductId = styled.span`
+
+`;
+
+export const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+`;
+
+export const ProductSize = styled.span`
+
+`;
+
+export const ProductPrice = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const ProductAmountConteiner = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+`;
+
+export const ProductAmount = styled.div`
+    font-size: 16px;
+    margin: 5px;
+`;
+
+export const ProductPriceDetail = styled.div`
+    font-size: 20px;
+
+`;
+
+export const Hr = styled.hr`
+    margin: 2rem;
+    background-color: ${({theme}) => theme.colors.secundary};
+    border: none;
+    height: 2px;
+`;
+
