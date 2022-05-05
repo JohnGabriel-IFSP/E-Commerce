f2346197db1e6184997282ecd72054cec44fbf1c
dijijn <nunes.jonathan@aluno.ifsp.edu.br>
2022-05-04 21:49:04 -0300

    Update main

--- src/components/Cart/CartStyle.tsx
+++ src/components/Cart/CartStyle.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const ContentConteiner = styled.div`
     display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
     padding: 0;
@@ -20,5 +21,25 @@ export const RightSide = styled.div`
 `;
 
 export const Title = styled.h1`
+
+`;
+
+export const InputCEP = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const DivFrete = styled.div`
+
+`;
+
+export const Div = styled.div`
+
+`;
+
+export const Text = styled.span`
 
 `;
